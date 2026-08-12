c849d61a145ad047493ea1c0d63c16d2218724d4
Danniel <danniel.ang8@gmail.com>
2026-08-12 10:10:08 +0700

    Initial Commit

---
+++ chal5-ios-app/chal5_ios_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  chal5_ios_appApp.swift
+//  chal5-ios-app
+//
+//  Created by Danniel on 12/08/26.
+//
+
+import SwiftUI
+
+@main
+struct chal5_ios_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
